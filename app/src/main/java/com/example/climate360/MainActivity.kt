0a7cc525625d46948d6f33b758c45b92ec3b8aff
cmanzi00 <cmanzi25@amherst.edu>
2023-11-13 13:44:23 -0500

    Version 3 Working recyclerview Working API calls Working Image and TextViews display Added more info toggler

--- app/src/main/java/com/example/climate360/MainActivity.kt
+++ app/src/main/java/com/example/climate360/MainActivity.kt
@@ -1,46 +1,20 @@
 package com.example.climate360
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-//    private fun getDogURL(){
-//        val client = AsyncHttpClient()
-//        client["https://dog.ceo/api/breed/labrador/images/random/20", object : JsonHttpResponseHandler()
-//        {
-//            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON)
-//            {
-//                val petImageArray = json.jsonObject.getJSONArray("message")
-////                val regex = Regex("/breeds/([^/]+)/")
-//
-//                for (i in 0 until petImageArray.length())
-//                {
-//                    val currLine = petImageArray.getString(i)
-//                    dogList.add(currLine)
-////                    val matchResult = regex.find(currLine)
-////                    val breed = matchResult?.groups?.get(1)?.value
-//
-//                }
-//                val adapter = DogAdapter(dogList)
-//                rvDogs.adapter = adapter
-//                rvDogs.layoutManager = LinearLayoutManager(this@MainActivity)
-//            }
-//
-//            override fun onFailure(
-//                statusCode: Int,
-//                headers: Headers?,
-//                errorResponse: String,
-//                throwable: Throwable?
-//            ) {
-//                Log.d("Dog Error", errorResponse)
-//            }
-//        }]
-//    }
+        val buttonActivity1: Button = findViewById(R.id.button1)
+
+        buttonActivity1.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
